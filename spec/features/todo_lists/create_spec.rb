cd827fd23cf363ef1993d53b28fa6e401a90073d
Adam Thede <adam@thedetech.com>
2015-06-02 19:12:04 -0500

    added a couple of tests :beers:

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Creating todo lists" do
 	it "redirects to the todo list index page on success" do
 		visit "/todo_lists"
-		click_link "New Todo list"
+		click_link "New"
 		expect(page).to have_content("New Todo List")
 
 		fill_in "Title", with: "My todo list"
@@ -17,7 +17,7 @@ describe "Creating todo lists" do
 		expect(TodoList.count).to eq(0)
 
 		visit "/todo_lists"
-		click_link "New Todo list"
+		click_link "New"
 		expect(page).to have_content("New Todo List")
 
 		fill_in "Title", with: ""
@@ -31,4 +31,24 @@ describe "Creating todo lists" do
 
 		expect(page).to_not have_content("This is what I'm doing today")
 	end
+
+	it "displays an error when the todo list has a title less than 3 characters" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New"
+		expect(page).to have_content("New Todo List")
+
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "This is what I'm doing today"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+
+		expect(page).to_not have_content("This is what I'm doing today")
+	end
+
 end
